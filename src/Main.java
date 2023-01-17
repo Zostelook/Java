7435ebc63d49f9eb605650fe4c91f1b5fc598264
abhishek <litecode420@gmail.com>
2023-01-17 12:34:23 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    int instanceVariable;   //non-static data member we can call it via an object
+    static double classVariable;  // This is static variable that have another name is class variable we can call it directly in static context without creating an object
+
+
+    public static void main(String[] args) {
+        Main ref=new Main();   // This is the object creation and "ref" is reference variable which contain reference id of an object
+
+        System.out.println(ref.instanceVariable);
+
+        System.out.println(classVariable);   // here is directly call for static variable
+    }
+}
