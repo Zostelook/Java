7e6bea15fd4465801d32dfa655ada2da5a68f3f4
abhishek <litecode420@gmail.com>
2023-01-18 11:32:38 +0530

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String ss[]){
+        String b ="false";
+        switch (b){
+            case "false":
+                System.out.println("a");
+        }
+    }
+}
