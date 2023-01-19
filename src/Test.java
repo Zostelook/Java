6110f429392cc01e50727fef819832e83404df18
abhishek <litecode420@gmail.com>
2023-01-19 20:44:06 +0530

    20230119

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String ss[]){
-        String b ="false";
-        switch (b){
-            case "false":
-                System.out.println("a");
-        }
+
+        System.out.println("Hello I am there,\nI am data ");
+        System.out.println("*\n**\n***\n****\n*****");
+
+        /* print the sum of three variable 3,6,and 9 without using int data type */
+        System.out.println(3+6+9);
+
+        /* But some programmer can write it something like this */
+        int a=3,b=6,c=9;
+        int sum = a+b+c;
+        System.out.println(sum);
+
+        // You can print the ASCII value of any character like this
+        System.out.println((int)'A');
+        System.out.println((int)'a');
+
+        // how to take input from user
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter an Integer number :");
+        int number = s.nextInt();
+        System.out.println("Entered number is :  " +number);
+
     }
 }
