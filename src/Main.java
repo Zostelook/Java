b17036230f06df5650e59cf44ee45fbc1c940e43
abhishek <litecode420@gmail.com>
2023-01-21 21:18:37 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,26 @@
 import java.util.*;
 public class Main {
 
-    static int i =0;
-    Main(){
-        System.out.println("This is constructor of Main class");
-    }
 
-    static {
-        System.out.println("It would be execute only once before the main method ");
-        // we can create an object of inside static block also, here constructor would be called automatically
-      Main ref=  new Main();
-
-        //
-
-    }
-
+ static void printAstrisksInTrianglurShap(int row,int col){
+     for(int i=1; i<=row; i++){
+         for(int j=1; j<=4-i; j++){
+             System.out.print("  ");
+         }
+         for(int j=1; j<=i; j++){
+             System.out.print(" *");
+         }
+         System.out.println();
+     }
+ }
 
 
-   static void printOddEven(int n){
-        for( i=1;i<=n;i++)
-        {
-            if(i%2==0)
-                System.out.print("Even ");
-            if(i%2!=0){
-                System.out.print("odd ");
-            }
-        }
-    }
-    static void printStringChbyCh(String name){
-       for( i=0;i<=name.length()-1;i++){
-           System.out.print(name.charAt(i));
-       }
-    }
 
-    static void reveserString(String str){
-        for( i=str.length()-1; i!=-1;i--){
-            System.out.println(str.charAt(i));
-        }
-    }
 
-    // how we can print ASCII value of any character
-    static void print_ASCII_Value(char firstSymbol,char second){
-        for( i= (int)firstSymbol;i<=(int)second;i++){
-            System.out.println(i);
-        }
-    }
     public static void main(String[] args) {
 
-     /*  Scanner sc = new Scanner(System.in);
-       System.out.print("Enter an Integer Number less than 20 :");
-       int input =sc.nextInt();
-       printOddEven(input);
-       */
 
+     printAstrisksInTrianglurShap(4,4);
 
-      printStringChbyCh("Abhishek Yadav");
-
-      /* from here you check actual size of String
-      String str = "Abhishek";
-      System.out.println(str.length());
-
-       */
-
-      reveserString("Infinite");
-      print_ASCII_Value('a','z');
-
-
-      // by the help of this you can directly print ASCII value
-      System.out.println((int)'z');
     }
 }
--- src/Newton.java
+++ src/Newton.java
@@ -1,4 +1,8 @@
 public class Newton {
 
-
+       static void asciiValueOfString(String str){
+           for(int i=0; i<str.length();i++){
+               //System.out.println("The value of "+(char)str[i]+" ");
+           }
+       }
 }
