8eb6eb9250144177ae5d07b57561421538828921
abhishek <litecode420@gmail.com>
2023-01-21 06:11:28 +0530

    again commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
+import java.util.*;
 public class Main {
-
-    int instanceVariable;   //non-static data member we can call it via an object
-
-    static double classVariable;  // This is static variable that have another name is class variable we can call it directly in static context without creating object
-
-
-
-
-    // Execution of program would be start from main() method , who one call main() - JVM call main method
+   static void printOddEven(int n){
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+                System.out.print("Even ");
+            else{
+                System.out.print("odd ");
+            }
+        }
+    }
     public static void main(String[] args) {
-        Main ref=new Main();   // This is the object creation and "ref" is reference variable which contain reference id of an object
 
-        System.out.println(ref.instanceVariable);
-
-        System.out.println(classVariable);   // here is directly call for static variable
-
-
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter an Integer Number less than 20 :");
+       int input =sc.nextInt();
+       printOddEven(input);
 
     }
 }
