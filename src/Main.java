fc577cf9c4b8f8bd4694e13180f7d784615eb392
abhishek <litecode420@gmail.com>
2023-01-25 00:52:07 +0530

    commits

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,19 @@
 public class Main {
 
-    static  int []Array = {2,3,4,5,6,7,8};
-   static int  size =0;
 
-
-    static void reveseArray(int [] Array, int start, int end){
-        int temp;
-        while (start<end){
-            temp =Array[start];
-            Array[start] = Array[end];
-            Array[end] = temp;
-            start++;
-            end--;
-        }
+     int x;
+     //   x=10;       we can't initialize like this in java
+    {
+        x=20;
     }
 
-    static void printArray(int [] Array , int size){
-        for(int i=0; i<size; i++){
-            System.out.print(Array[i]+" ");
-        }
+
+
+    public static void main(String ss[]){
+
         System.out.println();
-    }
-
-
-
-
-    public static void main(String[] args) {
-
-
-
-        printArray(Array,7);
-
-        reveseArray(Array,0,6);
-        System.out.println("Print Reverse Aarry :");
-        printArray(Array,7);
 
     }
+
 
 }
