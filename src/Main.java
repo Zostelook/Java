0d73c6b8c6293d13f56d2ea43f1fceacaee1094a
abhishek <litecode420@gmail.com>
2023-01-25 11:06:27 +0530

    commits

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
 
@@ -12,6 +14,21 @@ public class Main {
     public static void main(String ss[]){
 
         System.out.println(x);
+        System.out.println(("A"+"B"+'A'));
+
+       // difference between next() and nextLine();
+
+        Scanner sc = new Scanner(System.in);              // this object of scanner class
+
+        System.out.print("Enter your full name :");
+        String str1 = sc.next();                      // it will read only one word for example you gave input - Abhishek kumar , it will take only Abhishek
+        System.out.println(str1);
+
+
+     /*   System.out.println("Again Enter your full name:");
+        String str2 = sc.nextLine();                   it will read complete line 
+        System.out.println(str2);  */
+
 
     }
 
